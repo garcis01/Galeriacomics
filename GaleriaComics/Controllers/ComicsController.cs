0a71bb7998142ee1dfb0b58c66adcefc1e13c810
luis fernando garcia castro <luisgarcis01@gmail.com>
2017-11-30 18:44:25 -0600

    Proyecto ya con la vista

---
+++ GaleriaComics/Controllers/ComicsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GaleriaComics.Controllers
+{
+    public class ComicsController: Controller
+    {
+        /*public string Detalle()
+        {
+            return "mensaje desde el contolador";
+        }*/
+
+        public ActionResult Detalle()
+        {
+            return View();
+            /*if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult()
+            {
+                Content = "mensaje desde el controlador"
+            };*/
+        }
+    }
+}
